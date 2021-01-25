5c73dce998348afc22e81c0c6e7fc00e72344cd3
Anael Damette <damette.anael@gmail.com>
2021-01-25 11:41:29 +0100

    mise en place du panier sur le local storage

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,6 +1,14 @@
 //création constant url pour plus de facilité à l'appeler
 const url = "http://localhost:3000/api/furniture";
-
+class meuble {
+    constructor(name, price, imageUrl, selectVarnish, quantity) {
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.selectVarnish = selectVarnish;
+        this.quantity = quantity;
+    }
+}
 // création de la fonction : obtenir les meubles
 //variable nouvelle promesse
 let getAllMeuble = new Promise((resolve, reject) => {
@@ -92,6 +100,7 @@ let getOneMeuble = new Promise((resolve, reject) => {
 let pageArticle = document.getElementById("produitMeuble");
 if (pageArticle) {
     getOneMeuble.then((produitMeuble) => { showOneMeuble(produitMeuble) });
+
     function showOneMeuble(produitMeuble) {
         // construction carte pour le produit meuble
         let cardProduitMeuble = document.createElement("div");
@@ -128,16 +137,43 @@ if (pageArticle) {
         prixProduitCard.setAttribute('class', 'card-text text-justify');
         prixProduitCard.textContent = produitMeuble.price / 1000 + "€";
 
+        //mise en place du formulaire
+        let formulaire = document.createElement('form');
+        bodyProduitCard.appendChild(formulaire);
+
         //mise en place de l'option
-        let optionVarnish = document.createElement('form');
-        bodyProduitCard.appendChild(optionVarnish);
+        let optionVarnish = document.createElement('div');
+        formulaire.appendChild(optionVarnish);
         let titleOption = document.createElement('h5');
         optionVarnish.appendChild(titleOption);
         optionVarnish.textContent = "Vernis";
         let optionVernis = document.createElement('select');
         optionVarnish.appendChild(optionVernis);
         optionVernis.setAttribute('id', 'vernis');
+        optionVernis.setAttribute('class', 'formulaireProduit');
         optionVernis.setAttribute('name', 'varnish');
+
+        // mise en place de la quantité à mettre dans le panier 
+        let quantiteProduit = document.createElement('div');
+        formulaire.appendChild(quantiteProduit);
+        let titleQuantiteProduit = document.createElement('label');
+        quantiteProduit.appendChild(titleQuantiteProduit);
+        titleQuantiteProduit.textContent = 'Quantité';
+        let NbQuantiteProduit = document.createElement("input");
+        quantiteProduit.appendChild(NbQuantiteProduit);
+        NbQuantiteProduit.setAttribute('id', 'quantiteProduit');
+        NbQuantiteProduit.setAttribute('class', 'formulaireProduit');
+        NbQuantiteProduit.setAttribute('type', 'number');
+        NbQuantiteProduit.setAttribute('min', '1');
+        NbQuantiteProduit.setAttribute('step', '1');
+
+        //rajouter une fonction pour prendre la valeurQantiteProduit directement après la saisis de l'utilisateur!
+        let valeurQuantiteProduit;
+        NbQuantiteProduit.addEventListener("input", (e) => {
+            valeurQuantiteProduit = e.target.value;
+            valeurQuantiteProduit = parseFloat(valeurQuantiteProduit);
+            console.log(valeurQuantiteProduit, typeof (valeurQuantiteProduit));
+        });
 
         //création de la boucle foreach
         let selectionVernis = produitMeuble.varnish;
@@ -151,28 +187,40 @@ if (pageArticle) {
             couleurVernis.textContent = colorVernis;
         });
 
+
         //mise en place du bouton
-        let btnProduitCard = document.createElement("a")
-        bodyProduitCard.appendChild(btnProduitCard);
-        btnProduitCard.setAttribute('class', 'btn btn-primary')
-        btnProduitCard.setAttribute('id', 'setLocalStorage')
+        let btnProduitCard = document.createElement("button")
+        formulaire.appendChild(btnProduitCard);
+        btnProduitCard.setAttribute('class', 'btn btn-primary');
+        btnProduitCard.setAttribute('id', 'setLocalStorage');
+        btnProduitCard.setAttribute('type', 'submit');
         btnProduitCard.textContent = 'Mettre au panier';
 
         //création bouton addtocart
         let addToCart = document.getElementById('setLocalStorage');
         //déclenchement bouton addtocart uniquement sur la page produit
         if (addToCart) {
-            console.log("le bouton existe");
-            addToCart.addEventListener('click', () => {
+            addToCart.addEventListener('click', (event) => {
+                event.preventDefault();
+                console.log("le bouton existe");
+                let vernis = document.getElementById("vernis").value;
                 console.log("bouton add to cart")
-                cartNumbers();
-            })
+                let article = new meuble(produitMeuble.name, produitMeuble.price / 1000, produitMeuble.imageUrl, vernis, 0);
+                console.log(Number.isInteger(valeurQuantiteProduit));
+                console.log(valeurQuantiteProduit);
+                console.log(valeurQuantiteProduit > 0);
+                if (Number.isInteger(valeurQuantiteProduit) == true && valeurQuantiteProduit > 0) {
+                    setItems(article);
+                    //cartNumbers();
+                } else { alert("votre quantité doit être supérieur à zéro et être entière.") }
+            });
         }
-    };
+    }
 }
+
 //création bouton recycler
 let recyclePanier = document.getElementById("btnRecycle");
-console.log(recyclePanier);
+console.log("Bouton recycler");
 //déclenchement bouton recycle uniquement sur la page panier
 if (recyclePanier) {
     recyclePanier.addEventListener('click', () => {
@@ -181,25 +229,55 @@ if (recyclePanier) {
         onLoadFunction();
     })
 }
-
-//fonctionnalité : nombre d'objet dans le panier
-function cartNumbers() {
-    let productNumbers = localStorage.getItem('cartNumbers');
-    productNumbers = parseInt(productNumbers);
-    if (productNumbers) {
-        localStorage.setItem('cartNumbers', productNumbers + 1);
-        document.querySelector('.cart span').textContent = productNumbers + 1;
-    } else {
-        localStorage.setItem('cartNumbers', 1)
-        document.querySelector('.cart span').textContent = 1;
-    }
-}
+// Verification du nombre d'oject dans le panier lors d'un reload de page
 function onLoadFunction() {
     let productNumbers = localStorage.getItem('cartNumbers');
     if (productNumbers) {
         document.querySelector('.cart span').textContent = productNumbers;
-    }else {
+    } else {
         document.querySelector('.cart span').textContent = 0;
     }
+}
+/*fonctionnalité : nombre d'objet dans le panier
+function cartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    productNumbers = parseInt(productNumbers);
+
+    if (productNumbers) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+    } else {
+        
+    }
+}*/
+// Fonction : mettres les articles dans le local storage
+function setItems(article) {
+    let cartItems = localStorage.getItem('articleInCart');
+    let NbQuantiteProduit = document.getElementById('quantiteProduit').value;
+    let productNumbers = localStorage.getItem('cartNumbers');
+    productNumbers = parseInt(productNumbers);
+    NbQuantiteProduit = parseInt(NbQuantiteProduit);
+    cartItems = JSON.parse(cartItems); // récupération de ce qu'il y a dans articleInCart (notre panier)
+    if (cartItems != null) { // Si le panier n'est pas vide on le remplis
+        if (cartItems[article.name + ' ' + article.selectVarnish] == undefined) { // Si le nom de l'objet mis dans le panier est différent de celui déjà ajouté on rajoute le nouvel objet
+            localStorage.setItem('cartNumbers', productNumbers + 1);
+            document.querySelector('.cart span').textContent = productNumbers + 1;
+            cartItems = {
+                ...cartItems,
+                [article.name + ' ' + article.selectVarnish]: article
+            }
+
+        }
+        cartItems[article.name + ' ' + article.selectVarnish].quantity += NbQuantiteProduit; // on ajoute la nouvelle quantité à l'objet
+    } else { //on défini notre panier avec l'objet et la quantité à 1
+        article.quantity = NbQuantiteProduit;
+        localStorage.setItem('cartNumbers', 1)
+        document.querySelector('.cart span').textContent = 1;
+        cartItems = {
+            [article.name + ' ' + article.selectVarnish]: article
+        }
+    }
+    //on ajoute au local storage notre nouvel articleInCart
+    localStorage.setItem('articleInCart', JSON.stringify(cartItems));
 }
 onLoadFunction()
